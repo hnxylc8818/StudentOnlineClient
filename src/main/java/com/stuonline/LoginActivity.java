39b15a9296197303ce4e3edcd18dc25f4384f3c1
sjs <1716938123@qq.com>
2015-09-15 11:56:34 +0800

    孙继爽：创建了ValidateActivity

--- src/main/java/com/stuonline/LoginActivity.java
+++ src/main/java/com/stuonline/LoginActivity.java
@@ -2,14 +2,81 @@ package com.stuonline;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+import com.stuonline.https.MyCallBack;
+import com.stuonline.https.XUtils;
+import com.stuonline.utils.DialogUtil;
+import com.stuonline.views.CustomerEditText;
+import com.stuonline.views.TitleView;
 
 /**
  * Created by SunJiShuang on 2015/9/15.
  */
 public class LoginActivity extends Activity {
+    @ViewInject(R.id.login_title)
+    private TitleView title;
+    @ViewInject(R.id.login_account)
+    private CustomerEditText etAccount;
+    @ViewInject(R.id.login_pwd)
+    private CustomerEditText etPwd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        ViewUtils.inject(this);
+    }
+
+    @OnClick({R.id.login_login, R.id.login_reg, R.id.login_reset})
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_login:
+                //登录按钮
+                login();
+                break;
+            case R.id.login_reset:
+                //找回密码
+                break;
+            case R.id.login_reg:
+                //注册账号
+                break;
+        }
+    }
+
+    /**
+     * 登录方法
+     */
+    private void login() {
+        String account = etAccount.getText().toString().trim();
+        String pwd = etPwd.getText().toString().trim();
+        if (account.length() != 11) {
+            XUtils.showToast("账号格式错误，请重新输入");
+            return;
+        }
+        if (!pwd.matches("\"^\\\\w{6,20}$\"")) {
+            XUtils.showToast("密码格式错误，请重新输入");
+            return;
+        }
+        RequestParams params = new RequestParams();
+        params.addBodyParameter("u.account", account);
+        params.addBodyParameter("u.pwd", pwd);
+        DialogUtil.showWaitting(this);
+        XUtils.send(XUtils.LOGIN, params, new MyCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                DialogUtil.hiddenWaitting();
+                if (responseInfo != null) {
+                    String json = responseInfo.result;
+                }
+            }
+        });
     }
 }
