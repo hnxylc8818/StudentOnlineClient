1d819427c13f318e2ccbb3be0c6072efc19efd7e
swx <1049130780@qq.com>
2015-09-15 17:29:30 +0800

    孙卫星：注册Activity

---
+++ src/main/java/com/stuonline/AccountAndSecurityActivity.java
@@ -0,0 +1,15 @@
+package com.stuonline;
+
+import android.os.Bundle;
+
+/**
+ * 孙卫星：账号与安全
+ * Created by Administrator on 2015/9/15.
+ */
+public class AccountAndSecurityActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accoutandsecurity);
+    }
+}
---
+++ src/main/java/com/stuonline/UpdatePasswordActivity.java
@@ -0,0 +1,15 @@
+package com.stuonline;
+
+import android.os.Bundle;
+
+/**
+ * 孙卫星：修改密码
+ * Created by Administrator on 2015/9/15.
+ */
+public class UpdatePasswordActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updatepassword);
+    }
+}
