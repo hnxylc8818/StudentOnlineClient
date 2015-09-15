d91b7e8ce72b68ac32ccff3219718935cf4701ac
sjs <1716938123@qq.com>
2015-09-15 11:35:37 +0800

    孙继爽：创建了ValidateActivity

---
+++ src/main/java/com/stuonline/ValidateActivity.java
@@ -0,0 +1,15 @@
+package com.stuonline;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by SunJiShuang on 2015/9/15.
+ */
+public class ValidateActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_validate);
+    }
+}
