e69e1dbbef596f694960a41f2cd11b056deeb45c
swx <1049130780@qq.com>
2015-09-16 16:25:36 +0800

    孙卫星：个人信息

--- src/main/java/com/stuonline/PersonageMessageActivity.java
+++ src/main/java/com/stuonline/PersonageMessageActivity.java
@@ -1,6 +1,13 @@
 package com.stuonline;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.app.AlertDialog;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -9,9 +16,17 @@ import com.lidroid.xutils.ViewUtils;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
 import com.stuonline.https.XUtils;
+import com.stuonline.utils.DialogUtil;
 import com.stuonline.utils.SharedUtil;
 import com.stuonline.views.CircleImage;
 import com.stuonline.views.TitleView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * 孙卫星：个人信息
@@ -30,6 +45,22 @@ public class PersonageMessageActivity extends BaseActivity{
     private EditText mPersonSex;
     @ViewInject(R.id.pgemge_save)
     private Button mPersonSave;
+
+    private String[] items = { "拍照", "相册" };
+    private String title = "选择照片";
+    private static final int PHOTO_CARMERA = 1;
+    private static final int PHOTO_PICK = 2;
+    private static final int PHOTO_CUT = 3;
+    // 创建一个以当前系统时间为名称的文件，防止重复
+    private File tempFile = new File(Environment.getExternalStorageDirectory(),
+            getPhotoFileName());
+
+    // 使用系统当前日期加以调整作为照片的名称
+    private String getPhotoFileName() {
+        Date date = new Date(System.currentTimeMillis());
+        SimpleDateFormat sdf = new SimpleDateFormat("'PNG'_yyyyMMdd_HHmmss");
+        return sdf.format(date) + ".png";
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -51,6 +82,9 @@ public class PersonageMessageActivity extends BaseActivity{
                 break;
             case R.id.pgemge_photo:
                 //更改头像
+                AlertDialog.Builder dialog = DialogUtil.getListDialogBuilder
+                        (PersonageMessageActivity.this, items, title, dialogListener);
+                dialog.show();
                 break;
             case R.id.pgemge_save:
                 //保存
@@ -59,7 +93,128 @@ public class PersonageMessageActivity extends BaseActivity{
 
         }
     }
+    private DialogInterface.OnClickListener dialogListener=new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialog, int which) {
+            switch (which) {
+                case 0:
+                    // 调用拍照
+                    startCamera(dialog);
+                    break;
+                case 1:
+                    // 调用相册
+                    startPick(dialog);
+                    break;
 
+                default:
+                    break;
+            }
+        }
+    };
+    private void startPick(DialogInterface dialog) {
+        dialog.dismiss();
+        Intent intent = new Intent(Intent.ACTION_PICK, null);
+        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                "image/*");
+        startActivityForResult(intent, PHOTO_PICK);
+    }
+
+    private void startCamera(DialogInterface dialog) {
+        dialog.dismiss();
+        // 调用系统的拍照功能
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra("camerasensortype", 2); // 调用前置摄像头
+        intent.putExtra("autofocus", true); // 自动对焦
+        intent.putExtra("fullScreen", false); // 全屏
+        intent.putExtra("showActionIcons", false);
+        // 指定调用相机拍照后照片的存储路径
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(tempFile));
+        startActivityForResult(intent, PHOTO_CARMERA);
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case PHOTO_CARMERA:
+                startPhotoZoom(Uri.fromFile(tempFile), 300);
+                break;
+            case PHOTO_PICK:
+                if (null != data) {
+                    startPhotoZoom(data.getData(), 300);
+                }
+                break;
+            case PHOTO_CUT:
+                if (null != data) {
+                    setPicToView(data);
+                }
+                break;
+
+            default:
+                break;
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    /**
+     * 调用系统剪裁
+     * @param uri
+     * @param size
+     */
+    private void startPhotoZoom(Uri uri, int size) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        intent.setDataAndType(uri, "image/*");
+        // crop为true是设置在开启的intent中设置显示的view可以裁剪
+        intent.putExtra("crop", true);
+        // aspectX,aspectY是宽高的比例
+        intent.putExtra("aspectX", 1);
+        intent.putExtra("aspectY", 1);
+        // outputX,outputY是裁剪图片的宽高
+        intent.putExtra("outputX", size);
+        intent.putExtra("outputY", size);
+        // 设置是否返回数据
+        intent.putExtra("return-data", true);
+        startActivityForResult(intent, PHOTO_CUT);
+    }
+
+    /**
+     * 设置图片显示在界面
+     * @param data
+     */
+    private void setPicToView(Intent data) {
+        Bundle bundle = data.getExtras();
+        if (null != bundle) {
+            final Bitmap bmp = bundle.getParcelable("data");
+            mPersonPhoto.setImageBitmap(bmp);
+            saveCropPic(bmp);
+        }
+    }
+
+    /**
+     * 把剪裁完的图片保存到手机sdcard中
+     * @param bmp
+     */
+    private void saveCropPic(Bitmap bmp) {
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        FileOutputStream fis = null;
+        bmp.compress(Bitmap.CompressFormat.PNG, 100, baos);
+        try {
+            fis = new FileOutputStream(tempFile);
+            fis.write(baos.toByteArray());
+            fis.flush();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (null != baos) {
+                    baos.close();
+                }
+                if (null != fis) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
     @Override
     protected void onResume() {
         super.onResume();
--- src/main/java/com/stuonline/utils/DialogUtil.java
+++ src/main/java/com/stuonline/utils/DialogUtil.java
@@ -1,8 +1,10 @@
 package com.stuonline.utils;
 
-import android.app.AlertDialog;
+
 import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.app.AlertDialog;
 import android.view.Window;
 
 import com.stuonline.R;
@@ -12,6 +14,20 @@ import com.stuonline.R;
  */
 public class DialogUtil {
     public static Dialog waitting;
+    /**
+     * 返回一个列表对话框
+     * @param context   上下文
+     * @param items     列表项
+     * @param title     对话框标题
+     * @param clickListener 列表项点击监听
+     * @return
+     */
+    public static AlertDialog.Builder getListDialogBuilder(Context context,
+                                                           String[] items, String title,
+                                                           DialogInterface.OnClickListener clickListener) {
+        return new AlertDialog.Builder(context).setTitle(title).setItems(items, clickListener);
+
+    }
     /**
      * 显示进度对话框
      */
