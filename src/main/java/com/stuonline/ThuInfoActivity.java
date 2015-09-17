ff9e82ca4aee95b12f1a4f92a484430b625eff60
swx <1049130780@qq.com>
2015-09-17 11:12:17 +0800

    孙卫星：校院信息

--- src/main/java/com/stuonline/ThuInfoActivity.java
+++ src/main/java/com/stuonline/ThuInfoActivity.java
@@ -1,16 +1,29 @@
 package com.stuonline;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Base64;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TextView;
 
+import com.alibaba.fastjson.TypeReference;
 import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.http.RequestParams;
+import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
+import com.stuonline.entity.Muser;
+import com.stuonline.entity.Result;
+import com.stuonline.https.MyCallBack;
 import com.stuonline.https.XUtils;
+import com.stuonline.utils.DialogUtil;
+import com.stuonline.utils.JsonUtil;
 import com.stuonline.views.SelectPicPopupWindow;
 import com.stuonline.views.TitleView;
 
@@ -26,8 +39,17 @@ public class ThuInfoActivity extends BaseActivity {
     private SelectPicPopupWindow menuWindow;
     @ViewInject(R.id.thuinfo_role_tv)
     private TextView role;
+    @ViewInject(R.id.thuinfo_school_tv)
+    private TextView school;
+    @ViewInject(R.id.thuinfo_faculty_tv)
+    private TextView department;
+    @ViewInject(R.id.thuinfo_class_tv)
+    private TextView uclass;
     @ViewInject(R.id.thuinfo_date_tv)
     private TextView thuinfo_date_tv;
+
+    @ViewInject(R.id.thuinfo_bt)
+    private Button thuinfo_bt;
 
     private int roleId;
     private SimpleDateFormat df;
@@ -77,7 +99,7 @@ public class ThuInfoActivity extends BaseActivity {
         init();
     }
 
-    @OnClick({R.id.huinfo_role, R.id.thuinfo_school, R.id.thuinfo_faculty, R.id.thuinfo_class, R.id.thuinfo_date})
+    @OnClick({R.id.huinfo_role, R.id.thuinfo_school, R.id.thuinfo_faculty, R.id.thuinfo_class, R.id.thuinfo_date,R.id.thuinfo_bt})
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.huinfo_role:
@@ -106,7 +128,60 @@ public class ThuInfoActivity extends BaseActivity {
                         calendar.get(Calendar.DAY_OF_MONTH)
                 ).show();
                 break;
+            case R.id.thuinfo_bt:
+                saveThuInfo();
+                break;
         }
+    }
+
+    private void saveThuInfo() {
+//        String schools=school.getText().toString();
+        String schools="北科院";
+        String departments=department.getText().toString();
+        String ulcass=uclass.getText().toString();
+        String date=thuinfo_date_tv.getText().toString();
+        if (TextUtils.isEmpty(role.getText().toString())){
+            XUtils.showToast("请选择角色");
+            return;
+        }else if (TextUtils.isEmpty(schools)){
+            XUtils.showToast("请选择学校");
+            return;
+        }else if (TextUtils.isEmpty(departments)){
+            XUtils.showToast("请选择院系");
+            return;
+        }else if (TextUtils.isEmpty(ulcass)){
+            XUtils.showToast("请选择班级");
+            return;
+        }else if (TextUtils.isEmpty(date)){
+            XUtils.showToast("请选择入学年份");
+            return;
+        }
+
+        RequestParams params = new RequestParams();
+        params.addBodyParameter("u.uid", String.valueOf(MyApp.user.getUid()));
+        params.addBodyParameter("u.roleId", String.valueOf(roleId));
+        params.addBodyParameter("u.school", schools);
+        params.addBodyParameter("u.department", departments);
+        params.addBodyParameter("u.UClass",ulcass);
+        params.addBodyParameter("u.uyear", date);
+        DialogUtil.showWaitting(this);
+        XUtils.send(XUtils.UUSER, params, new MyCallBack<String>() {
+            @Override
+            public void onSuccess(ResponseInfo<String> responseInfo) {
+                DialogUtil.hiddenWaitting();
+                if (responseInfo != null) {
+                    JsonUtil<Result<Boolean>> jsonUtil = new JsonUtil<Result<Boolean>>(new TypeReference<Result<Boolean>>() {
+                    });
+                    Result<Boolean> result = jsonUtil.parse(responseInfo.result);
+                    XUtils.showToast(result.desc);
+                    if (result.state == Result.STATE_SUC) {
+                       finish();
+                        endIntentAnim();
+                    }
+                }
+            }
+        });
+
     }
 
     //为弹出窗口实现监听类
