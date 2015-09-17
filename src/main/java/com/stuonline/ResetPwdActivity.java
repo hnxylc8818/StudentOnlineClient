a931e07c812a8f0a5fcd3ee99b624e07fe7aa2e0
sjs <1716938123@qq.com>
2015-09-17 10:11:41 +0800

    孙继爽：修复意见反馈

---
+++ src/main/java/com/stuonline/ResetPwdActivity.java
@@ -0,0 +1,60 @@
+package com.stuonline;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.view.annotation.event.OnClick;
+
+/**
+ * Created by SunJiShuang on 2015/9/17.
+ * 找回密码
+ */
+public class ResetPwdActivity extends BaseActivity {
+    private Dialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        init();
+    }
+
+    private void init() {
+        setContentView(R.layout.activity_reset_pwd);
+        ViewUtils.inject(this);
+    }
+
+    @OnClick(R.id.reset_pwd_bt)
+    public void onClick(View v) {
+        dialog();
+    }
+
+    private void dialog() {
+        if (dialog == null) {
+            View v = LayoutInflater.from(ResetPwdActivity.this).inflate(
+                    R.layout.layout_repwd_dialog, null);
+            v.setBackgroundColor(Color.WHITE);
+            Button button = (Button) v.findViewById(R.id.select_dialog_helper_bt);
+            button.setOnClickListener(onClickListener);
+            AlertDialog.Builder builder = new AlertDialog.Builder(
+                    ResetPwdActivity.this);
+            builder.setView(v);
+            dialog = builder.show();
+            dialog.setCanceledOnTouchOutside(true);
+        } else {
+            dialog.show();
+        }
+    }
+
+    private View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            dialog.dismiss();
+        }
+    };
+}
