8a676f4ee79244b32ef71d205180cf3549cf34a1
swx <1049130780@qq.com>
2015-09-17 12:21:31 +0800

    孙卫星：校院信息

--- src/main/java/com/stuonline/ThuInfoActivity.java
+++ src/main/java/com/stuonline/ThuInfoActivity.java
@@ -1,11 +1,10 @@
 package com.stuonline;
 
 import android.app.DatePickerDialog;
-import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Base64;
+
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +17,7 @@ import com.lidroid.xutils.http.RequestParams;
 import com.lidroid.xutils.http.ResponseInfo;
 import com.lidroid.xutils.view.annotation.ViewInject;
 import com.lidroid.xutils.view.annotation.event.OnClick;
+
 import com.stuonline.entity.Muser;
 import com.stuonline.entity.Result;
 import com.stuonline.https.MyCallBack;
@@ -67,6 +67,20 @@ public class ThuInfoActivity extends BaseActivity {
         calendar = Calendar.getInstance();
         title = (TitleView) findViewById(R.id.thuinfo_title);
         title.setOnLeftclickListener(onClickListener);
+       if (MyApp.user!=null){
+           if (!TextUtils.isEmpty(String.valueOf(MyApp.user.getRoleId()))){
+               role.setText(MyApp.user.getRoleId() == 1 ? "学生" : "教师");
+           }else {
+               role.setText("未选择");
+           }
+
+           school.setText("北科院");
+           department.setText(MyApp.user.getDepartment());
+           uclass.setText(MyApp.user.getUClass());
+           thuinfo_date_tv.setText(MyApp.user.getUyear());
+       }
+
+
 
     }
 
@@ -170,11 +184,12 @@ public class ThuInfoActivity extends BaseActivity {
             public void onSuccess(ResponseInfo<String> responseInfo) {
                 DialogUtil.hiddenWaitting();
                 if (responseInfo != null) {
-                    JsonUtil<Result<Boolean>> jsonUtil = new JsonUtil<Result<Boolean>>(new TypeReference<Result<Boolean>>() {
+                    JsonUtil<Result<Muser>> jsonUtil = new JsonUtil<Result<Muser>>(new TypeReference<Result<Muser>>() {
                     });
-                    Result<Boolean> result = jsonUtil.parse(responseInfo.result);
+                    Result<Muser> result = jsonUtil.parse(responseInfo.result);
                     XUtils.showToast(result.desc);
                     if (result.state == Result.STATE_SUC) {
+                        MyApp.user=result.data;
                        finish();
                         endIntentAnim();
                     }
